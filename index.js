696aa3aa4c90654763d62f55300796c177603f49
DEVIKA SANTHOSH <devikasanthosh593@gmail.com>
2025-02-05 19:46:09 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const app = express();
 
-let persons = [
+const persons = [
     { id: "1", name: "Arto Hellas", number: "040-123456" },
     { id: "2", name: "Ada Lovelace", number: "39-44-5323523" },
     { id: "3", name: "Dan Abramov", number: "12-43-234345" },
     { id: "4", name: "Mary Poppendieck", number: "39-23-6423122" }
 ];
 
-// Existing GET route for persons
+// Route for /api/persons
 app.get('/api/persons', (req, res) => {
     res.json(persons);
 });
 
-// New /info route
+// Route for /info
 app.get('/info', (req, res) => {
+    const numEntries = persons.length;
     const currentTime = new Date();
-    const numberOfEntries = persons.length;
 
     res.send(`
-        <p>Phonebook has info for ${numberOfEntries} people</p>
+        <p>Phonebook has info for ${numEntries} people</p>
         <p>${currentTime}</p>
     `);
 });
